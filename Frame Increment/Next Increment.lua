21fdc47fa9f7715b6b3523c6ce71ddca3dbc53e3
Fahad Hasan Pathik <contact.fahadpathik@gmail.com>
2026-04-07 22:44:05 +0600

    Add files via upload

---
+++ Frame Increment/Next Increment.lua
@@ -0,0 +1,7 @@
+fusion = bmd.scriptapp("Fusion")
+comp = fusion.CurrentComp
+
+if not comp then return end
+
+local step = comp:GetData("FrameStep") or 10
+comp.CurrentTime = comp.CurrentTime + step
