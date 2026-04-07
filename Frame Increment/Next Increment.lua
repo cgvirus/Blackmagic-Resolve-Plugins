4d02e84a5753a267bf7daa47dafcda51ce305c6f
Fahad Hasan Pathik <contact.fahadpathik@gmail.com>
2026-04-07 22:55:15 +0600

    Delete Frame Increment directory

--- Frame Increment/Next Increment.lua
+++
@@ -1,7 +0,0 @@
-fusion = bmd.scriptapp("Fusion")
-comp = fusion.CurrentComp
-
-if not comp then return end
-
-local step = comp:GetData("FrameStep") or 10
-comp.CurrentTime = comp.CurrentTime + step
